2f3ce379a44cd66772ba049c0ddb14c6b18d5d71
Diogo Jerónimo <diogo10jeronimo@gmail.com>
2016-11-14 15:39:04 +0000

    Remove unused files, reset android app

--- SIRSApp/app/src/main/java/pt/utl/ist/sirs/t05/sirsapp/MainActivity.java
+++ SIRSApp/app/src/main/java/pt/utl/ist/sirs/t05/sirsapp/MainActivity.java
@@ -1,43 +1,21 @@
 package pt.utl.ist.sirs.t05.sirsapp;
 
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
 
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.security.PrivateKey;
-import java.security.PublicKey;
 
 public class MainActivity extends AppCompatActivity {
-
-    private ServerSocket serverSocket;
-    Handler updateConversationHandler;
-    private ServerThread serverThread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        updateConversationHandler = new Handler();
+        int forever = 1;
 
 
-        Thread thread = null;
-        this.serverThread = new ServerThread((TextView)findViewById(R.id.textView), updateConversationHandler);
 
-        thread = new Thread(serverThread);
-        thread.start();
-    }
-
-    @Override
-    protected void onStop(){
-        super.onStop();
-        try{
-            serverThread.getServerSocket().close();
-        }catch(IOException e){
-            e.printStackTrace();
-        }
+        System.exit(forever);
     }
 }
+

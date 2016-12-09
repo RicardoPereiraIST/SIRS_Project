7ebe3ac13e9a2dfb66cda14e3c8a97f718ad333f
Diogo Jerónimo <diogo10jeronimo@gmail.com>
2016-12-09 12:23:17 +0000

    Delete unused bash script

--- Scripts/mac_scan.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mac_address=$1
-
-if [[ -n "$mac_address" ]]; then
-   nmap -sP 192.168.1.0/24 | grep -B 2 $1 | sed -n '1p' | sed 's/.*(\(.*\))/\1/'
-else
-   echo "Usage: ./mac_scan.py <MAC_Address>"
-fi
